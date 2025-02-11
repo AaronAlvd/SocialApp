496367db89ba09bb951b52d0ccde9f82a464ac84
Aaron <offxcxal.aaron@icloud.com>
2025-02-10 19:50:31 -0800

    push dev

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App';
-import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from './store/session';
-import { ModalProvider, Modal, Modal2 } from './context/modal';
+import App from './App.jsx';
+import configureStore from './store/store.js';
+import { restoreCSRF, csrfFetch } from './store/csrf.js';
+import * as sessionActions from './store/session.js';
+import { ModalProvider, Modal, Modal2 } from './context/modal.jsx';
 import './index.css';
 
 (async () => {
