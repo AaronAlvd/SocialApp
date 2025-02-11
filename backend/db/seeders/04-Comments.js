95eb68b8a7f69778e9483217adc33f6ac068833f
Aaron <offxcxal.aaron@icloud.com>
2025-02-10 19:03:44 -0800

    update00

--- backend/db/seeders/04-Comments.js
+++ backend/db/seeders/04-Comments.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-   await Comment.bulkCreate([...comments1])
+   await Comment.bulkCreate([])
   },
 
   async down (queryInterface, Sequelize) {
