1e21e2eae08ce004b097d4c21fa961beee052d13
Aaron <offxcxal.aaron@icloud.com>
2025-02-10 17:15:04 -0800

    updated

--- backend/db/seeders/08-Messages.js
+++ backend/db/seeders/08-Messages.js
@@ -11,11 +11,14 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await Message.bulkCreate()
+    await Message.bulkCreate([])
   },
 
   async down (queryInterface, Sequelize) {
     await Message.destroy({
+      where: {
+        id: '1'
+      }
     })
   }
 };
